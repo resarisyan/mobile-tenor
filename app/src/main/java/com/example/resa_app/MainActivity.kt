ccd1d8cee9c19379097bad2e64bad80d5d07aefc
resarisyan <resarisyan77@gmail.com>
2022-12-23 11:20:52 +0700

    add logout activity

--- app/src/main/java/com/example/resa_app/MainActivity.kt
+++ app/src/main/java/com/example/resa_app/MainActivity.kt
@@ -3,9 +3,38 @@ package com.example.resa_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import android.content.Intent
+import androidx.core.content.edit
+import com.example.resa_app.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+    lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        auth = FirebaseAuth.getInstance()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.Simulasi.setOnClickListener {
+            val intent = Intent(this, ActivityTambah::class.java)
+            startActivity(intent)
+        }
+
+        binding.Logout.setOnClickListener{
+            auth.signOut()
+            val intent = Intent(this, ActivityLogin::class.java)
+            val preferences = getSharedPreferences("KEY_DATA", MODE_PRIVATE)
+            preferences.edit {
+                putString("email", "")
+                putString("password", "")
+            }
+            startActivity(intent)
+        }
+
     }
 }
